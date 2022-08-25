ced2975a91ca31b1d6525a34d78ff21a9bae6ac4
jmagistrado <joycemagistrado@gmail.com>
2022-08-25 15:55:13 -0700

    our project

--- paranormal_activity.rb
+++ paranormal_activity.rb
@@ -1,45 +1,77 @@
 # Introduction
-p 'You just moved into a new town of Salem, Massachusetts and are at home by yourself in the basement playing games. You notice that the lights start to flicker and the power goes out. You begin to hear unusual noises and a whisper of someone and the noise is getting louder. Do you want to hide in the bathroom or go upstairs?'
+class String
+    def print_slowly
+      self.each_char do |c|
+        sleep 0.001
+        print c
+      end
+    end
+  end
+
+  " ........ 
+;::;;::;, 
+;::;;::;;, 
+;;:::;;::;;, 
+.vnmmnv%vnmnv%,.;;;:::;;::;;,  .,vnmnv%vnmnv, 
+vnmmmnv%vnmmmnv%vnmmnv%;;;;;;;%nmmmnv%vnmmnv%vnmmnv 
+vnmmnv%vnmmmmmnv%vnmmmmmnv%;:;%nmmmmmmnv%vnmmmnv%vnmmmnv 
+vnmmnv%vnmmmmmnv%vnmmmmmmmmnv%vnmmmmmmmmnv%vnmmmnv%vnmmmnv 
+vnmmnv%vnmmmmmnv%vnmmmmmmmmnv%vnmmmmmmmmmmnv%vnmmmnv%vnmmmnv 
+vnmmnv%vnmmmmmnv%vnmm;mmmmmmnv%vnmmmmmmmm;mmnv%vnmmmnv%vnmmmnv, 
+vnmmnv%vnmmmmmnv%vnmm;' mmmmmnv%vnmmmmmmm;' mmnv%vnmmmnv%vnmmmnv 
+vnmmnv%vnmmmmmnv%vn;;    mmmmnv%vnmmmmmm;;    nv%vnmmmmnv%vnmmmnv 
+vnmmnv%vnmmmmmmnv%v;;      mmmnv%vnmmmmm;;      v%vnmmmmmnv%vnmmmnv 
+vnmmnv%vnmmmmmmnv%vnmmmmmmmmm;;       mmmmmmmmmnv%vnmmmmmmnv%vnmmmnv 
+vnmmnv%vnmmmmmmnv%vnmmmmmmmmmm;;     mmmmmmmmmmnv%vnmmmmmmnv%vnmmmnv 
+vnmmnv%vnmmmmm nv%vnmmmmmmmmmmnv;, mmmmmmmmmmmmnv%vn;mmmmmnv%vnmmmnv 
+vnmmnv%vnmmmmm  nv%vnmmmmmmmmmnv%;nmmmmmmmmmmmnv%vn; mmmmmnv%vnmmmnv 
+`vnmmnv%vnmmmm,  v%vnmmmmmmmmmmnv%vnmmmmmmmmmmnv%v;  mmmmnv%vnnmmnv' 
+vnmmnv%vnmmmm;,   %vnmmmmmmmmmnv%vnmmmmmmmmmnv%;'   mmmnv%vnmmmmnv 
+vnmmnv%vnmmmm;;,   nmmm;'              mmmm;;'    mmmnv%vnmmmmnv' 
+`vnmmnv%vnmmmmm;;,.         mmnv%v;,            mmmmnv%vnmmmmnv' 
+`vnmmnv%vnmmmmmmnv%vnmmmmmmmmnv%vnmmmmmmnv%vnmmmmmnv%vnmmmmnv' 
+`vnmvn%vnmmmmmmnv%vnmmmmmmmnv%vnmmmmmnv%vnmmmmmnv%vnmmmnv' 
+`vn%vnmmmmmmn%:%vnmnmmmmnv%vnmmmnv%:%vnmmnv%vnmnv'
+                                                     
+                                                            
+                                                                      
+".print_slowly
+
+class String
+    def prompt_slowly
+      self.each_char do |c|
+        sleep 0.07
+        print c
+      end
+    end
+  end
+
+'You just moved into a new town of Salem, Massachusetts and are at home by yourself in the basement playing games. You notice that the lights start to flicker and the power goes out. You begin to hear unusual noises and a whisper of someone and the noise is getting louder. Do you want to hide in the bathroom or go upstairs?'.prompt_slowly
 
 # haunted_user = gets.chomp
 answer = gets.chomp
 
 # def basement (basement_answer)
     if answer == 'bathroom'
-             puts 'Oh noooo, there was an evil ghost in the bathroom and they unfortunately killed you! GAME OVER!'
+             puts 'Oh noooo, there was an evil ghost in the bathroom and they unfortunately killed you! GAME OVER 💀!'
     elsif answer == 'upstairs'
             puts 'You have safely gone up the stairs, do you want to go to the into the kitchen or into the bedroom?'
-            else
-               puts "can't understand you, restart game 🤔"
-            upstairs_answer == gets.chomp
+            upstairs_answer = gets.chomp
     if upstairs_answer == 'kitchen'
-            puts 'You went into the kitchen and were met by the 👻 and you died!'
-            # kitchen_answer == gets.chomp
-            # if kitchen_answer == 3384
-            #       puts 'Thants correct! The ghost disappered and you are no longer hearing scary noises'  
-            # elsif kitchen_answer != 3384
-            #     puts 'Wrong answer, I am going to eat you! Bon appetit!' 
-
-            elsif upstairs_answer == 'bedroom'
-                puts "The ghost meets you in the bedroom and asks you this riddle, 'How do you eat an elphant?' enter you response below"
-                    bedroom_answer = gets.chomp
-                else
-                puts "can't understand you, restart game 🤔"
+            puts 'You went into the kitchen and were met by the ghost, you had a heart attack and died!💀'
+    elsif upstairs_answer == 'bedroom'
+            puts 'The ghost meets you in the bedroom and asks you this riddle: How do you eat an elephant?'
+                bedroom_answer = gets.chomp
     if bedroom_answer != 'one bite at a time'
-             puts 'You have answered incorrectly and the ghost killed you!Game Over 😢' 
+             puts 'You have answered incorrectly and the ghost killed you!' 
     elsif bedroom_answer == 'one bite at a time'
-        puts 'You answered correctly, I will spare your life. You can safely leave! 👻'
-    else
-        puts "can't understand you, restart game 🤔"
+        puts 'You are so funny and that is the correct answer! Today, I will spare your life 👻'
+    else 
+        puts 'I did not recognize that! Sorry, start over 😢'
+   
 
                 end
             end
         end
-    end
-    
+   
 
-
-
-
-    #  elsif answer == 'frontdoor'
-    #     puts 'The door is locked, we must turn around, go back to living room'
